f26bb2897b0f9aa7c4ba9b62e031af51cf68b9bf
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-03-10 01:00:19 +0530

    ADDED

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k=-k=%_2ziivlc&=r*m4+qe@^%@tss42+n$#9kv77b5qj96lh)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangocrudapp-production.up.railway.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
